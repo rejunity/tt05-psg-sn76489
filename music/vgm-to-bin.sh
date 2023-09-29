e45d179262d0258e4786161049826b27d6f3aae3
Renaldas Zioma <rej@unity3d.com>
2023-09-29 22:42:00 +0200

    Music conversion script

---
+++ music/vgm-to-bin.sh
@@ -0,0 +1,31 @@
+python ../../vgm-converter.git/vgmconverter.py MISSION76496.vgm -q 50 -t bbc -r MISSION76496.bbc50hz.bin
+python ../../vgm-converter.git/vgmconverter.py MISSION76496.vgm -q 50 -t bbc -o MISSION76496.bbc50hz.vgm
+
+python ../../vgm-converter.git/vgmconverter.py MISSION76496.vgm -q 60 -t ntsc -r MISSION76496.ntsc60hz.bin
+python ../../vgm-converter.git/vgmconverter.py MISSION76496.vgm -q 60 -t ntsc -o MISSION76496.ntsc60hz.vgm
+
+
+
+python ../../vgm-converter.git/vgmconverter.py DonkeyKongJunior-ingame.vgz -q 50 -t bbc -r DonkeyKongJunior-ingame.bbc50hz.bin
+python ../../vgm-converter.git/vgmconverter.py DonkeyKongJunior-ingame.vgz -q 50 -t bbc -o DonkeyKongJunior-ingame.bbc50hz.vgm
+
+python ../../vgm-converter.git/vgmconverter.py DonkeyKongJunior-ingame.vgz -q 60 -t ntsc -r DonkeyKongJunior-ingame.ntsc60hz.bin
+python ../../vgm-converter.git/vgmconverter.py DonkeyKongJunior-ingame.vgz -q 60 -t ntsc -o DonkeyKongJunior-ingame.ntsc60hz.vgm
+
+
+
+python ../../vgm-converter.git/vgmconverter.py 1942.vgm -q 50 -t bbc -r 1942.bbc50hz.bin
+python ../../vgm-converter.git/vgmconverter.py 1942.vgm -q 50 -t bbc -o 1942.bbc50hz.vgm
+
+python ../../vgm-converter.git/vgmconverter.py 1942.vgm -q 60 -t ntsc -r 1942.ntsc60hz.bin
+python ../../vgm-converter.git/vgmconverter.py 1942.vgm -q 60 -t ntsc -o 1942.ntsc60hz.vgm
+
+
+
+python ../../vgm-converter.git/vgmconverter.py CrazeeRider-title.vgz -q 50 -t bbc -r CrazeeRider-title.bbc50hz.bin
+python ../../vgm-converter.git/vgmconverter.py CrazeeRider-title.vgz -q 50 -t bbc -o CrazeeRider-title.bbc50hz.vgm
+
+python ../../vgm-converter.git/vgmconverter.py CrazeeRider-title.vgz -q 60 -t ntsc -r CrazeeRider-title.ntsc60hz.bin
+python ../../vgm-converter.git/vgmconverter.py CrazeeRider-title.vgz -q 60 -t ntsc -o CrazeeRider-title.ntsc60hz.vgm
+
+
