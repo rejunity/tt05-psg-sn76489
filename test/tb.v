fa106e5a21aea935297af14a42cf967d13ab683a
rej <rzioma@acm.org>
2024-11-02 23:37:18 +0100

    iHP0p2: updated files for iHP process

--- test/tb.v
+++ test/tb.v
@@ -1,51 +1,37 @@
 `default_nettype none
-`timescale 1ns/1ps
+`timescale 1ns / 1ps
 
-/*
-this testbench just instantiates the module and makes some convenient wires
-that can be driven / tested by the cocotb test.py
+/* This testbench just instantiates the module and makes some convenient wires
+   that can be driven / tested by the cocotb test.py.
 */
-
-// testbench is controlled by test.py
 module tb ();
 
-    // this part dumps the trace to a vcd file that can be viewed with GTKWave
-    initial begin
-        $dumpfile ("tb.vcd");
-        $dumpvars (0, tb);
-        #1;
-    end
+  // Dump the signals to a VCD file. You can view it with gtkwave.
+  initial begin
+    $dumpfile("tb.vcd");
+    $dumpvars(0, tb);
+    #1;
+  end
 
-    // wire up the inputs and outputs
-    wire [7:0] ui_in;
-    wire [7:0] uo_out;
-    wire [7:0] uio_in;
-    wire [7:0] uio_out;
-    wire [7:0] uio_oe;
-    wire clk;
-    wire rst_n;
-    wire ena;
-`ifdef GL_TEST
-    wire VPWR = 1'b1;
-    wire VGND = 1'b0;
-`endif
+  // Wire up the inputs and outputs:
+  reg clk;
+  reg rst_n;
+  reg ena;
+  reg [7:0] ui_in;
+  reg [7:0] uio_in;
+  wire [7:0] uo_out;
+  wire [7:0] uio_out;
+  wire [7:0] uio_oe;
 
-
-    tt_um_rejunity_sn76489 tt_um_rejunity_sn76489_uut 
-    (
-        // include power ports for the Gate Level test
-`ifdef GL_TEST
-        .VPWR(VPWR),
-        .VGND(VGND),
-`endif
-        .ui_in      (ui_in),    // Dedicated inputs
-        .uo_out     (uo_out),   // Dedicated outputs
-        .uio_in     (uio_in),   // IOs: Input path
-        .uio_out    (uio_out),  // IOs: Output path
-        .uio_oe     (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-        .ena        (ena),      // enable - goes high when design is selected
-        .clk        (clk),      // clock
-        .rst_n      (rst_n)     // not reset
-    );
+  tt_um_rejunity_sn76489 tt_um_rejunity_sn76489_uut (
+      .ui_in  (ui_in),    // Dedicated inputs
+      .uo_out (uo_out),   // Dedicated outputs
+      .uio_in (uio_in),   // IOs: Input path
+      .uio_out(uio_out),  // IOs: Output path
+      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
+      .ena    (ena),      // enable - goes high when design is selected
+      .clk    (clk),      // clock
+      .rst_n  (rst_n)     // not reset
+  );
 
 endmodule
